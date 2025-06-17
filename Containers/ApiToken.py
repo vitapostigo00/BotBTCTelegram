a94a784f290ebe050f504bcabba4189ac5d562a1
Vita <vita@uma.es>
2025-06-17 18:48:55 +0200

    Limpieza final repositorio

---
+++ Containers/ApiToken.py
@@ -0,0 +1,8 @@
+def returnApiToken ():
+    return 'Aqui va el Api Token'
+
+def btcMainnetPass ():
+    return "Aqui va la contraseña del nodo de mainnet"
+
+def btcTestnetPass ():
+    return "Aqui va la contraseña del nodo de testnet"
